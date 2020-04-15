c5daad6759fb9753f0792c8a211b3f92d96266d7
Nick Boyd <nickjohnboyd@gmail.com>
2020-04-15 08:22:34 -0600

    initial file structure set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrailSearchComponent } from './trail-search/trail-search.component';
+import { TrailListComponent } from './trail-list/trail-list.component';
+import { TrailItemComponent } from './trail-item/trail-item.component';
+import { TrailDetailsComponent } from './trail-details/trail-details.component';
+import { UserTrailsComponent } from './user-trails/user-trails.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrailSearchComponent,
+    TrailListComponent,
+    TrailItemComponent,
+    TrailDetailsComponent,
+    UserTrailsComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
