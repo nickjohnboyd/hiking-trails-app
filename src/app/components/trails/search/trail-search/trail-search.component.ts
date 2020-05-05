ee20ea25f8592e5db1dcfc93c58f350da4bf47b0
adakai <adakai@github.com>
2020-05-05 07:46:04 -0600

    added back the wrong zip for trails list

--- src/app/components/trails/search/trail-search/trail-search.component.ts
+++ src/app/components/trails/search/trail-search/trail-search.component.ts
@@ -1,27 +1,21 @@
-import { ZipService } from './../../../../shared/zip.service';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-trail-search',
-  templateUrl: './trail-search.component.html',
-  styleUrls: ['./trail-search.component.scss']
+  selector: "app-trail-search",
+  templateUrl: "./trail-search.component.html",
+  styleUrls: ["./trail-search.component.scss"],
 })
 export class TrailSearchComponent implements OnInit {
   zip: number;
 
-  constructor(
-    private zipService: ZipService,
-    private router: Router,
-  ) { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-  }
-
-  search() {
-    // this.validZip = this.zipService.zipValidator(this.zip);
-    this.router.navigate([`trails/${this.zip}`]);
     
   }
 
+  search() {
+    this.router.navigate([`trails/${this.zip}`]);
+  }
 }
